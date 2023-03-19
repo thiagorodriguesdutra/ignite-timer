60da1a22e0ece3cec908d6ce03fb66278da3c1f9
thiagodutra <thiagorsd@icloud.com>
2023-03-19 14:38:57 -0300

    Add component Routes and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './components/Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secundary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
-      <Button></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
