55a6e5e6b013ddd6b424fc7bc3db68ee27022579
thiagodutra <thiagorsd@icloud.com>
2023-03-21 20:58:52 -0300

    add css to History component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,37 +19,49 @@ export function History() {
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Relatório matinal</td>
               <td>26 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
